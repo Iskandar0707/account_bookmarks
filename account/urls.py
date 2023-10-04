bb1c758ba23deb0de8d580c5473ad4eab24480c3
Iskandar <you@example.com>
2023-10-05 01:37:27 +0500

    tugadi

--- account/urls.py
+++ account/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('login/',auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
-    
+    path('pasword_change/',auth_views.PasswordChangeView.as_view(),name='password_change'),
+    path('pasword_change/done/',auth_views.PasswordChangeDoneView.as_view(),name='password_change')
+
 
 ]
