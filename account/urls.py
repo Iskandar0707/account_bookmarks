bd3b06ca4d1d1392294ffb90ba25f588e9ef5b39
Iskandar <you@example.com>
2023-10-06 22:39:05 +0500

    register

--- account/urls.py
+++ account/urls.py
@@ -1,14 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from account import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/',views.user_login,name='login'),
-    # path('login/',auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', views.dashboard, name='dashboard'),
-    path('pasword_change/',auth_views.PasswordChangeView.as_view(),name='password_change'),
-    path('pasword_change/done/',auth_views.PasswordChangeDoneView.as_view(),name='password_change')
-
-
+    path('' , views.dashboard, name='dashboard'),
+    path('', include('django.contrib.auth.urls')),
+    path('register/',views.register,name='register')
 ]
--- account/views.py
+++ account/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate,login
-from .forms import LoginForm
+from .forms import LoginForm,UserRegistrationForm
 from django.contrib.auth.decorators import login_required
+
+def register(request) :
+    if request.method == 'POST' :
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid() :
+            new_user = user_form.save(commit=False)
+            new_user.set_password(
+                user_form.cleaned_data['password']
+            )
+            new_user.save()
+            return render(request, 'account/register_done.html',
+                          {'new_user' : new_user}
+                          )
+    else :
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form' : user_form})
 
 def user_login(request) :
     if request.method == 'POST' :
@@ -12,10 +28,10 @@ def user_login(request) :
             user = authenticate(request,
                                 username=cd['username'],
                                 password=cd['password'])
-        if user is not None :
-            if user.is_active :
-                login(request,user)
-                return HttpResponse('Auth seccessfully')
+            if user is not None :
+                if user.is_active :
+                    login(request,user)
+                    return HttpResponse('Auth seccessfully')
             else :
                 return HttpResponse('Disamble account')
         else:
